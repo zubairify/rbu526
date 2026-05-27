863f90a9e459ce90227dca6bb3d7b402381ec498
zubairify <41644191+zubairify@users.noreply.github.com>
2026-05-27 12:33:11 +0530

    Library service updated

--- library-service/src/main/java/com/rbu/controller/BookRestController.java
+++ library-service/src/main/java/com/rbu/controller/BookRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.rbu.entity.Book;
 import com.rbu.service.BookService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookRestController {
